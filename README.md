3ea8c2cfe0d427cb496594b09281c7bec0e2e21a
Saida Dhanavath <dhana.sai@gmail.com>
2017-02-07 13:02:43 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Managing threads in Java
---
+++ src/edu/adias/java/threads/HelloThread.java
@@ -0,0 +1,11 @@
+package edu.adias.java.threads;
+
+public class HelloThread extends Thread{
+     public void run() {
+        System.out.println("Hello from "+Thread.currentThread().getName()
+                + " created by "
+                + "extending Thread class!");
+    }
+
+    
+}
